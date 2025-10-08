dd8160317286861f302769490a48a358113a43ae
smitbahadure1 <bahaduresmit08@gmail.com>
2025-10-08 19:16:11 +0530

    Fixed build paths for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    outDir: 'dist'
+  }
 });
